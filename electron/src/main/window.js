1d2765ccaf03d0cbbb6d70e6cc6c4b0df8d41958
TJ <pagecow@gmail.com>
2026-04-10 06:03:27 -0500

    saving updates

--- electron/src/main/window.js
+++ electron/src/main/window.js
@@ -1,7 +1,15 @@
+const fs = require("fs");
 const path = require("path");
 const { BrowserWindow } = require("electron");
 
 let mainWindow = null;
+
+function getWindowIconPath() {
+  if (process.platform !== "linux") return undefined;
+
+  const iconPath = path.join(__dirname, "../../../build/icon.png");
+  return fs.existsSync(iconPath) ? iconPath : undefined;
+}
 
 function createMainWindow(startUrl) {
   const win = new BrowserWindow({
@@ -11,6 +19,7 @@ function createMainWindow(startUrl) {
     minHeight: 700,
     backgroundColor: "#f7f8f4",
     title: "PageCow",
+    icon: getWindowIconPath(),
     autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, "../../preload.js"),
